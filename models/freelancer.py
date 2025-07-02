c3996a5cbf7679d723880addddda7a1394892043
luizhtmoreira <lht.unb@gmail.com>
2025-07-02 16:39:47 -0300

    Revert "feat: Implementa arquitetura base do backend com modelos e controllers"
    This reverts commit c62a4537fda7eae53ce8632f7dc807e8ebaea846.

--- models/freelancer.py
+++
@@ -1,6 +0,0 @@
-# models/freelancer.py
-from models.usuario import Usuario
-
-class Freelancer(Usuario):
-    def __init__(self, nome: str, email: str, id: int = None):
-        super().__init__(nome, email, 'freelancer', id)
